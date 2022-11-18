91ad5680b324800ce310be09e1fcb91639850816
Ryan A <alumkalryan@gmail.com>
2022-11-17 21:29:05 -0500

    added more to casinogame

--- Casino Game/casinogame.cpp
+++ Casino Game/casinogame.cpp
@@ -1,26 +1,37 @@
+
 #include <iostream>
 #include <random>
+#include <ctime>
 using namespace std;
 
 // Make it a slots game, 3 random ascii images are shown, if 3 of the same, grant points and ask user if they want to continue
 
-int points(int value){
-	int number = 1+ (rand()%10);
-	int score =0;
-	cout<< number;
-	if (value == number){
-		score +=1;
-		}
-	return score;	
+string symbols(){
+	string symbols[3] = {"6","7","8"};
+	string result = "";
+	for (int i =0; i <3; i++)
+		result = result + symbols[rand()%3];
+	return result;
+}
+
+int point(string result, int value){
+	string number = result[0];
+	int point;
+	if (number == result[1] && number == result[2])
+		point = 1.5* value;
+	return point;
+	
 }
 
 int main(){
+	srand(time(NULL));
 	int value;
-	cout<<"Welcome to the Casino";
-	cout <<"\nEnter a number between 1-10: "; 
+	cout<<"\nWelcome to the Casino \n \nEnter your bet and play slots!";
+	cout <<"\nEnter your bet: "; 
 	cin>>value;
-	int score = points(value);
-	cout<< "\nYour score is "<< score;
+	string result = symbols();
+	point(result, value);
+	cout<< "\nResults: " <<result;
 	return 0;
 	
 }
