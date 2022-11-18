66e1ccf6350a6f731823020660726a76293e5831
Ryan A <82676662+ryanalumkal@users.noreply.github.com>
2022-11-18 10:27:49 -0500

    Finished basic slots and betting

--- Casino Game/casinogame.cpp
+++ Casino Game/casinogame.cpp
@@ -6,32 +6,43 @@ using namespace std;
 
 // Make it a slots game, 3 random ascii images are shown, if 3 of the same, grant points and ask user if they want to continue
 
-string symbols(){
-	string symbols[3] = {"6","7","8"};
+int point(string result, int bet)
+{
+	char number = result[0];
+	int point;
+	if (number == result[1] && number == result[2])
+		point = ((1.5)* bet);
+	return point;
+}
+
+string symbols()
+{
+	char symbols[3] = {'6','7','8'};
 	string result = "";
 	for (int i =0; i <3; i++)
 		result = result + symbols[rand()%3];
 	return result;
 }
 
-int point(string result, int value){
-	string number = result[0];
-	int point;
-	if (number == result[1] && number == result[2])
-		point = 1.5* value;
-	return point;
-	
-}
-
-int main(){
-	srand(time(NULL));
-	int value;
+int main()
+{
+	int bet, cash_out = 0;
+	char user_continue;
 	cout<<"\nWelcome to the Casino \n \nEnter your bet and play slots!";
-	cout <<"\nEnter your bet: "; 
-	cin>>value;
-	string result = symbols();
-	point(result, value);
-	cout<< "\nResults: " <<result;
+	while (true)
+	{
+		srand(time(NULL));
+		cout <<"\nEnter your bet: "; 
+		cin>>bet;
+		string result = symbols();
+		cout << "\n" <<result;
+		cash_out += point(result, bet);
+		cout<< "\nYour winnings: " << cash_out;
+		cout<<"\nDo you want to continue? (Enter Y or N): ";
+		cin>> user_continue;
+		if (user_continue == 'N' || user_continue == 'n')
+			break;
+	}
 	return 0;
 	
-}
+}	
