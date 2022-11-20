a628516e787ead309dcf7a8c3ac5eb875bb8e598
Ryan A <alumkalryan@gmail.com>
2022-11-20 14:45:33 -0500

    Added comments and cleaned up code

--- Casino Game/casinogame.cpp
+++ Casino Game/casinogame.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+
 using namespace std;
 
-int point(string result, int bet)
+int point(string result, int bet,int &bet_total)
+// Calculates winnings or losses
 {
 	char number = result[0];
 	int point = 0;
 	if (number == result[1] && number == result[2])
 		point = ((1.5)* bet);
-	return point;
+	if (point == 0)
+	{
+		bet_total -= bet;
+	}
+	else
+	{
+		bet_total += point;	
+	}
+	return bet_total;
 }
 
 string symbols()
+// Slots machine output
 {
 	char symbols[3] = {'6','7','8'};
 	string result = "";
@@ -21,29 +32,24 @@ string symbols()
 	return result;
 }
 
-int main()
+int main() 
+// main program
 {
-	int bet_total =0;
+	
+	int bet_total = 0;
 	cout<<"\nWelcome to the Casino \n \nEnter your bet and play slots!";
 	while (true)
+	// while user wants to continue playing
 	{
 		int cash_out = 0, bet =0;
 		srand(time(NULL));
 		cout <<"\nEnter your bet: "; 
 		cin>>bet;
-		string result = symbols();
+		string result = symbols(); 
+		// slot machine output
 		cout << "\n" <<result;
-		cash_out = point(result, bet);
-		if (cash_out == 0)
-		{
-			bet_total -= bet;
-		}
-		else
-		{
-			bet_total += cash_out;	
-		}
-			
-		cout<< "\nYour total winnings: " << bet_total;
+		cout<< "\nYour total winnings: " << point(result, bet, bet_total);
+		// prints total winnings or losses
 		cout<<"\nDo you want to continue? (Enter Y or N): ";
 		try
 		{
@@ -52,11 +58,13 @@ int main()
 			if (user_continue == 'N' || user_continue == 'n'|| user_continue == 'Y' || user_continue == 'y')
 			{
 				if (user_continue == 'N' || user_continue == 'n')
+				// if user wants to end program
 				{
 					cout<<"Thank you for gambling!";
 					break;
 				}
 			}else{
+			// if user input is incorrect
 				throw "incorrect value";
 			}
 		}
@@ -66,3 +74,4 @@ int main()
 		}
 	}
 }	
+// END
